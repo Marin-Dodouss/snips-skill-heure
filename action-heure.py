2a1068bee38a1d288748bf12eed593e2a3b49d32
Marin-Dodouss <43744866+Marin-Dodouss@users.noreply.github.com>
2019-05-01 17:49:21 +0200

    Update action-heure.py

--- action-heure.py
+++ action-heure.py
@@ -3,6 +3,10 @@ from hermes_python.hermes import Hermes
 from datetime import datetime
 from pytz import timezone
 
+import locale
+locale.setlocale(locale.LC_TIME,'')
+'French_France.1252'
+
 MQTT_IP_ADDR = "localhost"
 MQTT_PORT = 1883
 MQTT_ADDR = "{}:{}".format(MQTT_IP_ADDR, str(MQTT_PORT))
@@ -42,18 +46,24 @@ def intent_received(hermes, intent_message):
 	print()
 	print(intent_message.intent.intent_name)
 	print ()
-
+	
 	if intent_message.intent.intent_name == 'Joseph:askTime':
 
+		now = datetime.now(timezone('Europe/Paris'))
+		day=now.strftime(" %A %d")
+		month=now.strftime(" %B")
+		year=now.strftime(" %Y")
+		sentence_date='Nous sommes le '
+		sentence_date+=day+month+year+"."
+		print(sentence_date)
+		
 		sentence = 'Il est '
 		print(intent_message.intent.intent_name)
-
-		now = datetime.now(timezone('Europe/Paris'))
 
 		sentence += verbalise_hour(now.hour) + " " + verbalise_minute(now.minute)
 		print(sentence)
 
-		# hermes.publish_continue_session(intent_message.session_id, sentence, ["Joseph:greetings"])
+		hermes.publish_end_session(intent_message.session_id, sentence_date)
 		hermes.publish_end_session(intent_message.session_id, sentence)
 
 	elif intent_message.intent.intent_name == 'Joseph:greetings':
